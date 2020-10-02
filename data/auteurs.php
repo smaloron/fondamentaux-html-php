9b377a20ade70113bdd22132d11f1e34789cf14d
Sebastien Maloron <contact@smaloron.eu>
2020-10-02 14:50:53 +0200

    Exercices PHP sur les boucles et les conditions

---
+++ data/auteurs.php
@@ -0,0 +1,18 @@
+<?php
+
+$auteurs = [
+    ["name" => "Aristote", "country" => "Greece", "alive" => false],
+    ["name" => "Paul Auster", "country" => "USA", "alive" => true],
+    ["name" => "Virgine Despentes", "country" => "France", "alive" => true],
+    ["name" => "Ovide", "country" => "Romain", "alive" => false],
+    ["name" => "Jorge Luis Borges", "country" => "Argentine", "alive" => false],
+    ["name" => "Cervantes", "country" => "Espagne", "alive" => false]
+];
+
+/* A faire
+
+- Afficher les noms des auteurs dans une liste
+- Mettre en gras les auteurs vivants
+- Afficher les données des auteurs dans un tableau html
+- N'afficher que les auteurs morts
+*/
